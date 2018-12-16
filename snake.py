b4019857353f3580b85f8ffcb2c4291b25c92b1d
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2018-12-16 20:58:44 +0200

    ceva mai okay

--- snake.py
+++ snake.py
@@ -1,163 +1,142 @@
 from ple.games.snake import Snake
 from ple import PLE
 import pygame
-import keras
 import numpy as np
+import keras
 from keras.models import Sequential
-from keras.layers import InputLayer, Dense
-
+from keras.layers import Dense
+import math
 
 class MyAgent:
     def __init__(self):
         self.model = Sequential()
-        # head.x/size, head.y/size, fruit.x/size, fruit.y/size,
-        # min(d(head,body)), no.segments
-        self.model.add(InputLayer((6,)))
-        self.model.add(Dense(18, activation="sigmoid"))
-        self.model.add(Dense(3, activation="softmax"))
-        # opt = SGD(lr=0.01)
-        self.model.compile(loss="categorical_crossentropy",
-                           optimizer='adam', metrics=["accuracy"])
+        self.model.add(Dense(9, input_dim=4, activation="relu"))
+        self.model.add(Dense(15, activation="relu"))
+        self.model.add(Dense(3))
+        sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(sgd, "mse")
 
     def train(self, train_set, target_set):
-        self.model.fit(train_set.reshape(1, 6),
-                       target_set.reshape(1, 3), verbose=0)
+        self.model.fit(x=train_set, y=target_set, epochs=1, verbose=0)
 
     def getAction(self, state):
-        # print(state)
-        # print(self.model.layers[0].get_weights())
-        return self.model.predict(state.reshape(1, 6))
+        return self.model.predict(state.reshape(1, 4))
+
+    def train_batch(self, train_set):
+        y = 0.95
+        target_set = []
+        for current_state, action, reward, new_state in train_set:
+            target = reward + y * np.max(agent.getAction(new_state))
+            target_vector = agent.getAction(current_state)[0]
+            target_vector[action] = target
+            target_set.append(target_vector)
+        train_set = [i[0] for i in train_set]
+        self.train(np.array(train_set), np.array(target_set))
 
 
-def getDistance(state):
-    distance = abs(state[0] - state[2]) + abs(state[1] - state[3])
+# get action from currently_available_actions + index:
+def nextAction(agent,epsilon,current_state,currently_available_actions):
+    if np.random.random() < epsilon:
+        index = np.random.randint(0, 3)
+        next_action = currently_available_actions[index]
+    else:
+        index = np.argmax(agent.getAction(current_state))
+        next_action = currently_available_actions[index]
+    return next_action, index
+
+def closestSegmentDistance(current_state):
+    distance = 50000000
+    for point in current_state["snake_body_pos"]:
+        relative_distance = abs(point[0] - current_state["snake_head_x"]) + abs(point[1] - current_state["snake_head_y"])
+        if  relative_distance < distance:
+            distance = relative_distance
     return distance
 
+def closestWallDistance(current_state,screen_size):
+    distances = []
+    distances.append(current_state["snake_head_x"])
+    distances.append(current_state["snake_head_y"])
+    distances.append(abs(screen_size-current_state["snake_head_x"]))
+    distances.append(abs(screen_size-current_state["snake_head_y"]))
+    distances = np.array(distances)
+    return np.min(distances)
 
-def getInput(state, screenDimension):
+def getInput(current_state,screen_size):
     inputData = []
-    inputData.append(state["snake_head_x"] / screenDimension)
-    inputData.append(state["snake_head_y"] / screenDimension)
-    inputData.append(state["food_x"] / screenDimension)
-    inputData.append(state["food_y"] / screenDimension)
-    inputData.append(min(state["snake_body"][2:]))
-    angle = 0
-    if state["snake_head_x"] - state["food_x"] > 0:
-        angle += 1
-    if state["snake_head_y"] - state["food_y"] > 0:
-        angle += 2
-    # inputData.append(len(state["snake_body"]))
-    inputData.append(angle)
+    # positionare relativa la snake
+    relative_x = np.sign(current_state["food_x"] - current_state["snake_head_x"])
+    relative_y = np.sign(current_state["food_y"] - current_state["snake_head_y"])
+    food_distance = abs(current_state["food_x"] - current_state["snake_head_x"]) +\
+                    abs(current_state["food_y"] - current_state["snake_head_y"])
+    inputData.append(relative_x)
+    inputData.append(relative_y)
+    inputData.append(food_distance)
+    # cea mai apropiata coliziune
+    if len(current_state["snake_body_pos"]) >= 7:
+        if closestSegmentDistance(current_state) < closestWallDistance(current_state,screen_size):
+            inputData.append(closestSegmentDistance(current_state))
+        else:
+            inputData.append(closestWallDistance(current_state,screen_size))
+    else:
+        inputData.append(closestWallDistance(current_state,screen_size))
     return np.array(inputData)
 
 
-screenDims = 150
-game = Snake(screenDims, screenDims, 3)
-p = PLE(game, fps=30, display_screen=True)
+screen_size = 150
+game = Snake(screen_size, screen_size, 3, )
+p = PLE(game, fps=15, display_screen=True, force_fps=True)
 
 agent = MyAgent()
 
 game.init()
-# reward = 0.0
-
 game.clock = pygame.time.Clock()
-# curr_dist = 0
-state = getInput(game.getGameState(), screenDims)
-# actios board directions X0Y: {
-#     119: up
-#     97: left
-#     100: right
-#     115: down
-# }
-# snake direction:
-# 0: right
-# 1: left
-# 2: down
-# 3: up
 
-# 302-right
-# 213-left
-# 021-down
-# 130-up
-directions = [[3, 0, 2], [2, 1, 3], [0, 2, 1], [1, 3, 0]]
-
-''' NN parameters '''
-game.rewards["tick"] = -1.0
-game.rewards["loss"] = -150.0
-game.rewards["win"] = 150.0
-y = 0.9
-epsilon = 0.6
-iteration = 0
-train_set = []
-actions = list(game.getActions())
-# turn left, forward, turn right
-available_actions = directions[0]
+# initialisation part:
+epsilon = 0.9
+batch_size = 50
+actions = sorted(list(game.getActions()))  # left, right, down, up
+all_possible_actions = [[2,0,3], [3,1,2], [1,2,0], [0,3,1]]  # for left, right, down, up
+currently_available_actions = all_possible_actions[1]
+current_state = getInput(game.getGameState(),screen_size)
+training_set = []
+iterations = 0
+reset_flag = False
 
 while True:
-    if game.game_over():
+    if game.game_over() or reset_flag:
         game.init()
+        available_actions = all_possible_actions[1]
+        current_state = getInput(game.getGameState(),screen_size)
+        reset_flag = False
+		iterations = 0
+
     dt = game.clock.tick_busy_loop(30)
-    if np.random.random() < epsilon:
-        next_action = np.random.randint(0, 3)
-    else:
+    # get the action and its reward, compute the new currently_available_actions:
+    action, index = nextAction(agent,epsilon,current_state,currently_available_actions)
+    reward = p.act(actions[action])
+    currently_available_actions = all_possible_actions[action]
 
-        next_action = np.argmax(agent.getAction(state))
-    # Act
-    # pare ciudat? dar merge
-    reward = p.act(actions[available_actions[next_action]])
     # Next step
-    # pare ciudat dar merge
-    available_actions = directions[available_actions[next_action]]
     game.step(dt)
-    new_state = getInput(game.getGameState(), screenDims)
-    if getDistance(state) > getDistance(new_state):
-        reward += 1
-    target = reward + y * np.max(agent.getAction(new_state))
-    target_set = agent.getAction(state)[0]
-    target_set[next_action] = target
-    agent.train(state, target_set)
-    state = new_state
-    iteration += 1
-    epsilon *= 0.999
-    # train_set.append(getInput(game.getGameState(),screenDims))
-    # if iteration==500:
-    #     agent.train(train_set)
-    #     train_set.clear()
+    new_state = getInput(game.getGameState(),screen_size)
+
+    if iterations > 300:
+        iterations = 0
+        reset_flag = True
+        reward = game.rewards["loss"]
+        
+    if reward > 0:
+        iterations = 0
+
+    experience = [current_state, index, reward, new_state]
+    training_set.append(experience)
+
+    # send training data:
+    if len(training_set) == batch_size:
+        print("Train")
+        agent.train_batch(training_set)
+        training_set.clear()
+
+    current_state = new_state
+    iterations += 1
     pygame.display.update()
-
-# OUR JOB LOOKS LIKE THIS
-# --------------------------------------------------------------
-# game = FlappyBird()
-# p = PLE(game, fps=30, display_screen=True)
-# agent = myAgentHere(allowed_actions=p.getActionSet())  <-- RN
-#
-# p.init()
-# reward = 0.0
-#
-# for i in range(nb_frames):
-#     if p.game_over():
-#         p.reset_game()
-#
-#     observation = p.getScreenRGB()
-#     action = agent.pickAction(reward, observation)
-#     reward = p.act(action)
-
-
-# import pygame
-# import numpy as np
-#
-# pygame.init()
-# game = Snake(width=128, height=128)
-# game.screen = pygame.display.set_mode( game.getScreenDims(), 0, 32)
-# game.clock = pygame.time.Clock()
-# game.rng = np.random.RandomState(24)
-# game.init()
-#
-# while True:
-#     if game.game_over():
-#         game.init()
-#
-#     dt = game.clock.tick_busy_loop(30)
-#     game.step(dt)
-#     pygame.display.update()
-#     print(game.getGameState())
