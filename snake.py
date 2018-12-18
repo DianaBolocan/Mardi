01c96c4321b9630327cf3f95b6301a901be1717b
Avram Marius Alexandru <mrsavra@gmail.com>
2018-12-18 21:30:23 +0200

    final version (broken)

--- snake.py
+++ snake.py
@@ -3,24 +3,31 @@ from ple import PLE
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 
 
 class MyAgent:
     def __init__(self):
-        self.model = keras.models.load_model("snake_model.h5")
+        self.train_counter = 0
+        self.model = Sequential()
+        self.model.add(Dense(9, input_dim=4, activation="relu"))
+        self.model.add(Dense(15, activation="relu"))
+        self.model.add(Dense(3, activation="linear"))
+        sgd = keras.optimizers.SGD(
+            lr=0.009, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(sgd, "mse", metrics=["accuracy"])
 
     def train(self, train_set, target_set):
         self.model.fit(x=train_set, y=target_set, epochs=1, verbose=0)
-        print(self.model.layers[2].get_weights()[1])
         self.train_counter += 1
+        print(self.model.layers[2].get_weights()[1])
         if self.train_counter == 200:
             print("CHECKPOINT")
             self.model.save('snake_model.h5')
             self.train_counter = 0
 
     def getAction(self, state):
-        return self.model.predict(state.reshape(1, 4))
+        return self.model.predict(state.reshape(1, 4))[0]
 
     def train_batch(self, train_set):
         y = 0.9
@@ -30,7 +37,7 @@ class MyAgent:
                 target = reward
             else:
                 target = reward + y * np.max(agent.getAction(new_state))
-            target_vector = agent.getAction(current_state)[0]
+            target_vector = agent.getAction(current_state)
             target_vector[action] = target
             target_set.append(target_vector)
         train_set = [i[0] for i in train_set]
@@ -39,13 +46,12 @@ class MyAgent:
 
 # get action from currently_available_actions + index:
 def nextAction(agent, epsilon, current_state, currently_available_actions):
-    # if np.random.random() < epsilon:
-    #     index = np.random.randint(0, 3)
-    #     next_action = currently_available_actions[index]
-    # else:
-    index = np.argmax(agent.getAction(current_state))
-    print(agent.getAction(current_state))
-    next_action = currently_available_actions[index]
+    if np.random.random() < epsilon:
+        index = np.random.randint(0, 3)
+        next_action = currently_available_actions[index]
+    else:
+        index = np.argmax(agent.getAction(current_state))
+        next_action = currently_available_actions[index]
     return next_action, index
 
 
@@ -72,39 +78,36 @@ def closestWallDistance(current_state, screen_size):
 def getInput(current_state, screen_size):
     inputData = []
     # positionare relativa la snake
-    relative_x = np.sign(
-        current_state["food_x"] - current_state["snake_head_x"])
-    relative_y = np.sign(
-        current_state["food_y"] - current_state["snake_head_y"])
+    relative_x = current_state["food_x"] - current_state["snake_head_x"]
+    relative_y = current_state["food_y"] - current_state["snake_head_y"]
     food_distance = abs(current_state["food_x"] - current_state["snake_head_x"]) +\
         abs(current_state["food_y"] - current_state["snake_head_y"])
-    inputData.append((relative_x+1.0)/2)
-    inputData.append((relative_y+1.0)/2)
+    inputData.append(relative_x)
+    inputData.append(relative_y)
     inputData.append(food_distance)
     # cea mai apropiata coliziune
-    # if len(current_state["snake_body_pos"]) >= 7:
-    #     if closestSegmentDistance(current_state) < closestWallDistance(current_state, screen_size):
-    #         inputData.append(closestSegmentDistance(current_state))
-    #     else:
-    #         inputData.append(closestWallDistance(current_state, screen_size))
-    # else:
-    inputData.append(closestWallDistance(current_state, screen_size))
+    if len(current_state["snake_body_pos"]) >= 7:
+        if closestSegmentDistance(current_state) < closestWallDistance(current_state, screen_size):
+            inputData.append(closestSegmentDistance(current_state))
+        else:
+            inputData.append(closestWallDistance(current_state, screen_size))
+    else:
+        inputData.append(closestWallDistance(current_state, screen_size))
     return np.array(inputData)
 
 
 screen_size = 150
-game = Snake(screen_size, screen_size, 3, )
-p = PLE(game, fps=15, display_screen=True, force_fps=True)
+game = Snake(screen_size, screen_size, 3)
+p = PLE(game, fps=30, display_screen=True, force_fps=True)
 
 agent = MyAgent()
 
 p.init()
-
 # initialisation part:
-game.rewards["loss"] = -2
-game.rewards["positive"] = 1.01
-epsilon = 0.9
-batch_size = 300
+game.rewards["loss"] = -1
+game.rewards["positive"] = 1
+epsilon = 0.7
+# batch_size = 300
 actions = sorted(list(game.getActions()))  # left, right, down, up
 all_possible_actions = [[2, 0, 3], [3, 1, 2], [
     1, 2, 0], [0, 3, 1]]  # for left, right, down, up
@@ -120,58 +123,50 @@ prev_score = current_score
 prev_epsilon = epsilon
 while True:
     if game.game_over() is True or reset_flag:
-        # if prev_score > 0:
-        #     if np.random.random() < prev_score:
-        #         print(epsilon)
-        #         prev_epsilon = epsilon
-        #         agent.train_batch(training_set)
-        #     else:
-        #         epsilon = prev_epsilon
-        # elif np.random.random() < 0.01:
-        #     print(epsilon)
-        #     prev_epsilon = epsilon
-        #     agent.train_batch(training_set)
-        # else:
-        #     epsilon = prev_epsilon
-        # training_set.clear()
+        if prev_score > 0:
+            # print(epsilon)
+            prev_epsilon = epsilon
+            agent.train_batch(training_set)
+        elif np.random.random() < 0.01:
+            # print(epsilon)
+            prev_epsilon = epsilon
+            agent.train_batch(training_set)
+        else:
+            epsilon = prev_epsilon
+        training_set.clear()
         p.init()
         available_actions = all_possible_actions[1]
-        # current_state = getInput(game.getGameState(), screen_size)
+        current_state = getInput(game.getGameState(), screen_size)
         reset_flag = False
         iterations = 0
         game_over_flag = True
+        maxscore = 0
 
-    dt = game.clock.tick_busy_loop(30)
+    # dt = game.clock.tick_busy_loop(30)
     # get the action and its reward, compute the new currently_available_actions:
     action, index = nextAction(
         agent, epsilon, current_state, currently_available_actions)
     reward = p.act(actions[action])
     currently_available_actions = all_possible_actions[action]
-    #
-    # # Next step
-    # # game.step(dt)
+
+    # Next step
+    # game.step(dt)
     new_state = getInput(game.getGameState(), screen_size)
-    #
-    # if iterations > 100:
-    #     iterations = 0
-    #     reset_flag = True
-    #     reward = game.rewards["loss"]
-    # if reward > 0:
-    #     iterations = 0
-    # prev_score = current_score
-    # current_score = game.getScore()
-    # if current_score > maxscore:
-    #     maxscore = current_score
-    #     print(maxscore)
-    # # if current_score > prev_score:
-    # #     # print(current_score)
-    # #     pass
-    # # else:
-    # if current_state[2] < new_state[2]:
-    #     reward += (-current_state[2] / screen_size) / 150
-    # # print(reward)
-    # experience = [current_state, index, reward, new_state, game.game_over()]
-    # training_set.append(experience)
+
+    if iterations > 100:
+        iterations = 0
+        reset_flag = True
+        reward = game.rewards["loss"]
+    if reward > 0:
+        iterations = 0
+    prev_score = current_score
+    current_score = game.getScore()
+    if current_score > maxscore:
+        maxscore = current_score
+    if current_state[0] < new_state[0]:
+        reward += (-current_state[0] / screen_size)
+    experience = [current_state, index, reward, new_state, game.game_over()]
+    training_set.append(experience)
     current_state = new_state
-    # iterations += 1
-    # epsilon *= 0.9999
+    iterations += 1
+    epsilon *= 0.9999
--- snakeTest.py
+++ snakeTest.py
@@ -8,14 +8,7 @@ from keras.layers import Dense
 
 class MyAgent:
     def __init__(self):
-        self.train_counter = 0
-        self.model = Sequential()
-        self.model.add(Dense(9, input_dim=2, activation="relu"))
-        self.model.add(Dense(15, activation="relu"))
-        self.model.add(Dense(3, activation="linear"))
-        sgd = keras.optimizers.SGD(
-            lr=0.009, decay=1e-6, momentum=0.9, nesterov=True)
-        self.model.compile(sgd, "mse", metrics=["accuracy"])
+        self.model = keras.models.load_model("snake_model.h5")
 
     def train(self, train_set, target_set):
         self.model.fit(x=train_set, y=target_set, epochs=1, verbose=0)
@@ -27,7 +20,7 @@ class MyAgent:
             self.train_counter = 0
 
     def getAction(self, state):
-        return self.model.predict(state.reshape(1, 2))
+        return self.model.predict(state.reshape(1, 2))[0]
 
     def train_batch(self, train_set):
         y = 0.9
@@ -37,7 +30,7 @@ class MyAgent:
                 target = reward
             else:
                 target = reward + y * np.max(agent.getAction(new_state))
-            target_vector = agent.getAction(current_state)[0]
+            target_vector = agent.getAction(current_state)
             target_vector[action] = target
             target_set.append(target_vector)
         train_set = [i[0] for i in train_set]
@@ -46,12 +39,13 @@ class MyAgent:
 
 # get action from currently_available_actions + index:
 def nextAction(agent, epsilon, current_state, currently_available_actions):
-    if np.random.random() < epsilon:
-        index = np.random.randint(0, 3)
-        next_action = currently_available_actions[index]
-    else:
-        index = np.argmax(agent.getAction(current_state))
-        next_action = currently_available_actions[index]
+    # if np.random.random() < epsilon:
+    #     index = np.random.randint(0, 3)
+    #     next_action = currently_available_actions[index]
+    # else:
+    index = np.argmax(agent.getAction(current_state))
+    print(agent.getAction(current_state))
+    next_action = currently_available_actions[index]
     return next_action, index
 
 
@@ -74,8 +68,6 @@ def closestWallDistance(current_state, screen_size):
     distances = np.array(distances)
     return np.min(distances)
 
-def closestWallDirection(current_state, screen_size, direction):
-    
 
 def getInput(current_state, screen_size):
     inputData = []
@@ -86,17 +78,17 @@ def getInput(current_state, screen_size):
         current_state["food_y"] - current_state["snake_head_y"])
     food_distance = abs(current_state["food_x"] - current_state["snake_head_x"]) +\
         abs(current_state["food_y"] - current_state["snake_head_y"])
-    # inputData.append(relative_x)
-    # inputData.append(relative_y)
+    # inputData.append((relative_x+1.0)/2)
+    # inputData.append((relative_y+1.0)/2)
     inputData.append(food_distance)
     # cea mai apropiata coliziune
-    if len(current_state["snake_body_pos"]) >= 7:
-        if closestSegmentDistance(current_state) < closestWallDistance(current_state, screen_size):
-            inputData.append(closestSegmentDistance(current_state))
-        else:
-            inputData.append(closestWallDistance(current_state, screen_size))
-    else:
-        inputData.append(closestWallDistance(current_state, screen_size))
+    # if len(current_state["snake_body_pos"]) >= 7:
+    #     if closestSegmentDistance(current_state) < closestWallDistance(current_state, screen_size):
+    #         inputData.append(closestSegmentDistance(current_state))
+    #     else:
+    #         inputData.append(closestWallDistance(current_state, screen_size))
+    # else:
+    inputData.append(closestWallDistance(current_state, screen_size))
     return np.array(inputData)
 
 
@@ -111,7 +103,7 @@ p.init()
 # initialisation part:
 game.rewards["loss"] = -2
 game.rewards["positive"] = 1.01
-epsilon = 0.7
+epsilon = 0.9
 batch_size = 300
 actions = sorted(list(game.getActions()))  # left, right, down, up
 all_possible_actions = [[2, 0, 3], [3, 1, 2], [
@@ -128,55 +120,58 @@ prev_score = current_score
 prev_epsilon = epsilon
 while True:
     if game.game_over() is True or reset_flag:
-        if prev_score > 2:
-            print(epsilon)
-            prev_epsilon = epsilon
-            agent.train_batch(training_set)
-        elif np.random.random() < 0.01:
-            print(epsilon)
-            prev_epsilon = epsilon
-            agent.train_batch(training_set)
-        else:
-            epsilon = prev_epsilon
-        training_set.clear()
+        # if prev_score > 0:
+        #     if np.random.random() < prev_score:
+        #         print(epsilon)
+        #         prev_epsilon = epsilon
+        #         agent.train_batch(training_set)
+        #     else:
+        #         epsilon = prev_epsilon
+        # elif np.random.random() < 0.01:
+        #     print(epsilon)
+        #     prev_epsilon = epsilon
+        #     agent.train_batch(training_set)
+        # else:
+        #     epsilon = prev_epsilon
+        # training_set.clear()
         p.init()
         available_actions = all_possible_actions[1]
-        current_state = getInput(game.getGameState(), screen_size)
+        # current_state = getInput(game.getGameState(), screen_size)
         reset_flag = False
         iterations = 0
         game_over_flag = True
 
-    # dt = game.clock.tick_busy_loop(30)
+    dt = game.clock.tick_busy_loop(30)
     # get the action and its reward, compute the new currently_available_actions:
     action, index = nextAction(
         agent, epsilon, current_state, currently_available_actions)
     reward = p.act(actions[action])
     currently_available_actions = all_possible_actions[action]
-
-    # Next step
-    # game.step(dt)
+    #
+    # # Next step
+    # # game.step(dt)
     new_state = getInput(game.getGameState(), screen_size)
-
-    if iterations > 100:
-        iterations = 0
-        reset_flag = True
-        reward = game.rewards["loss"]
-    if reward > 0:
-        iterations = 0
-    prev_score = current_score
-    current_score = game.getScore()
-    if current_score > maxscore:
-        maxscore = current_score
-        print(maxscore)
-    # if current_score > prev_score:
-    #     # print(current_score)
-    #     pass
-    # else:
-    if current_state[0] < new_state[0]:
-        reward += (-current_state[0] / screen_size)
-    # print(reward)
-    experience = [current_state, index, reward, new_state, game.game_over()]
-    training_set.append(experience)
+    #
+    # if iterations > 100:
+    #     iterations = 0
+    #     reset_flag = True
+    #     reward = game.rewards["loss"]
+    # if reward > 0:
+    #     iterations = 0
+    # prev_score = current_score
+    # current_score = game.getScore()
+    # if current_score > maxscore:
+    #     maxscore = current_score
+    #     print(maxscore)
+    # # if current_score > prev_score:
+    # #     # print(current_score)
+    # #     pass
+    # # else:
+    # if current_state[2] < new_state[2]:
+    #     reward += (-current_state[2] / screen_size) / 150
+    # # print(reward)
+    # experience = [current_state, index, reward, new_state, game.game_over()]
+    # training_set.append(experience)
     current_state = new_state
-    iterations += 1
-    epsilon *= 0.9999
+    # iterations += 1
+    # epsilon *= 0.9999
